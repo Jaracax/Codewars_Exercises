35a356843878f147fece31dad351cc57a95f4c1b
Jose Antonio Dominguez <jadominguez99@gmail.com>
2023-07-18 20:23:14 -0300

    Exercise Complete

---
+++ [01] 8 Kyu/get_character_from_ASCII_Value/src/main/java/org/example/Solution.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/*Instructions:
+* Write a function which takes a number and returns the corresponding ASCII char for that value.
+* */
+public class Solution {
+
+    public static char getChar(int c) {
+        return (char) (int) c;
+    }
+}
